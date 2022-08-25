b3f0f50f69df79476089c57f665e5f501ffd7eae
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-08-25 11:49:16 -0300

    criação dos schemas e exportação do modulo

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -29,3 +29,19 @@ const debtSchema = new mongoose.Schema({
 })
 
 // Schema para o ciclo de pagamento 
+// ele terá 5 atributos NOME, MES, ANO, LISTA DE CREDITOS E LISTA DEBITOS
+// NOME String, requerido,
+// MES Number, min 1 max 12, requerido
+// ANO Number, min 1970, max 2100, requerido
+// CREDITS apontado para o creditSchema criado anteriormente
+// DEBTS apontado para o debtSchema criado anteriormente
+const billingCycleSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    month: {type: Number, min: 1, max: 12, required: true },
+    year: {type: Number, min: 1970, max: 2100, required: true},
+    credits: [creditSchema],
+    debts: [debtSchema]
+})
+
+// Exportar o modulo colocando o billingCycleSchema para dizer como ele sera persistido no mongoDB
+module.exports = restful.model('BillingCycle', billingCycleSchema)
