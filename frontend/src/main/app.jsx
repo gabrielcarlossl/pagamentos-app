0f2142ec71906821a6e339a4b206f2a33fa2ee54
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-08-30 15:22:44 -0300

    criado index e app

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function App (props) {
+       return ( // classe do admin LTE Wrapper significa uma div que envolve todo o site
+        <div className='wrapper'>
+            <h1>App</h1>
+        </div>)
+    }
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,33 +1,25 @@
-// Configuração do WebPack
-
-const webpack = require('webpack') // Importando o webpack
-
-const ExtractTextPlugin = require('extract-text-webpack-plugin') // Importar o arquivo responsavel pelos CSS e aplicar 
-
-// Configuração abaixo do webpack,
-// primeiro configurando a entrada ENTRY e saida OUTPUT
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.jsx', 
+    entry: './src/index.jsx',
     output: {
         path: __dirname + '/public',
-        filename: './app.js' 
+        filename: './app.js'
     },
-    // configurando webserver
     devServer: {
         port: 8080,
-        contentBase: './public', // Pasta onde ele vai ler o conteudo
+        contentBase: './public',
     },
     resolve: {
         extensions: ['', '.js', '.jsx'],
-        alias: { // criando apelido para arquivos
-            modules: __dirname + '/node_modules', // sempre que for usar a pasta node_modules basta colocar modules
-            jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js', // Criar apelido ao jquery e bootstrap, admin lte usa
+        alias: {
+            modules: __dirname + '/node_modules',
+            jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
             bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
-    // Configuração dos plugins, referencias para o jquery
-    plugins: [
+    plugins: [ 
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
@@ -37,18 +29,18 @@ module.exports = {
     ],
     module: {
         loaders: [{
-            test:/.js[x]?$/,
+            test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
-            query:{
-                preset: ['es2015', 'react'],
+            query: {
+                presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
-        },{
+        }, {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
-            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/, // fontes
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
             loader: 'file'
         }]
     }
