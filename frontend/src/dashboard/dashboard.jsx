e51233763ab7f71886bccfdad1bd081eee7769e0
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-09-01 14:12:22 -0300

    adicionado componente valueBox dentro do dashboard

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -2,13 +2,18 @@ import React, {Component} from "react";
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
 
 class Dashboard extends Component {
     render(){
         return(
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0'></ContentHeader>
-                <Content>Dashboard</Content>
+                <Content>
+                    <ValueBox col='12 4' color='green' icon='bank' value='R$ 10' text='Total de Créditos'></ValueBox>
+                    <ValueBox col='12 4' color='red' icon='credit-card' value='R$ 10' text='Total de Débitos'></ValueBox>
+                    <ValueBox col='12 4' color='blue' icon='money' value='R$ 0' text='Valor Consolidado'></ValueBox>
+                </Content>
             </div>
         )
     }
