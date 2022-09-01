b9b2f2e9d19562d1d2a58d991301a2c78f1f512f
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-09-01 10:16:34 -0300

    estrutura base da aplicação

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,8 +1,14 @@
+import '../common/template/dependencies'
 import React from "react";
+
+import Header from '../common/template/header';
+import Sidebar from '../common/template/sidebar';
 
 export default function App (props) {
        return ( // classe do admin LTE Wrapper significa uma div que envolve todo o site
         <div className='wrapper'>
-            <h1>App</h1>
+            <Header></Header>
+            <Sidebar></Sidebar>
+
         </div>)
     }
