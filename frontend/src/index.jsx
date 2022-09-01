7a93202fde79a02c18d567f9dc572407acc5fa0a
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-09-01 15:06:18 -0300

    adicionado Provider envolvendo o app

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,16 @@
 import React from "react";
-import ReactDOM  from "react-dom";
-import App from "./main/app";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App/>, document.getElementById('app'))
+import App from "./main/app";
+import reducers from './main/reducers'
+
+// o store é o estado unico da aplicação, gerado pelas combinações dos reducers
+// a aplicação fica integrada com o redux depois de envolver ela pelo PROVIDER 
+const store = createStore(reducers)
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById("app"));
