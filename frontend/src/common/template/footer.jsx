1decc40ea7d11d45cb7bae8705dfcb5db2bf4015
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-09-01 10:24:13 -0300

    adicionado componente footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2022
+            <a href="https://github.com/gabrielcarlossl" target='_blank'> Gabriel Carlos.</a>
+        </strong>
+    </footer>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,12 +3,17 @@ import React from "react";
 
 import Header from '../common/template/header';
 import Sidebar from '../common/template/sidebar';
+import Footer from '../common/template/footer'
 
 export default function App (props) {
        return ( // classe do admin LTE Wrapper significa uma div que envolve todo o site
         <div className='wrapper'>
             <Header></Header>
             <Sidebar></Sidebar>
+            <div className='content-wrapper'>
+                <h1>Conteudo</h1>
+            </div>
+            <Footer></Footer>
 
         </div>)
     }
