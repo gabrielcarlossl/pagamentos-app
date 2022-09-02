5a9bb8c2e0f56661cb842976a212945c24b37fb2
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-09-02 13:27:28 -0300

    adicionado middleware e mudança no store

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { applyMiddleware ,createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
 import promise from "redux-promise";
 
 import App from "./main/app";
-import reducers from './main/reducers'
+import reducers from "./main/reducers";
 
 // o store é o estado unico da aplicação, gerado pelas combinações dos reducers
-// a aplicação fica integrada com o redux depois de envolver ela pelo PROVIDER 
+// a aplicação fica integrada com o redux depois de envolver ela pelo PROVIDER
 
-const store = applyMiddleware(promise)(createStore)(reducers)
+const store = applyMiddleware(promise)(createStore)(reducers);
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-, document.getElementById("app"));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
