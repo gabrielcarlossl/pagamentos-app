dcd62d6a4023de75079a5885dd9fb83041c774cf
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-09-05 14:28:53 -0300

    adição do li, link e configuração do component TabHeader

---
+++ frontend/src/common/tab/tabHeader.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+//Estrutura para renderizar uma unica aba
+class TabHeader extends Component {
+    render(){
+        return(
+                        //  
+            <li>
+                <a href data-toggle='tab' data-target={this.props.target}>
+                    <i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
+                </a>
+            </li>
+        )
+    }
+}
+
+export default TabHeader
