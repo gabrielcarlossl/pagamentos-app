05ca4b90470c86bea79bd780219ec96b195021eb
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-09-05 14:13:36 -0300

    adicionado components dentro do billingCycle e criação do tabHeader

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,13 +1,25 @@
 import React, {Component} from "react";
+
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
+import Tabs from "../common/tab/tabs";
+import TabsHeader from "../common/tab/tabsHeader";
+import TabsContent from "../common/tab/tabsContent";
 class BillingCycle extends Component {
     render(){
         return(
             <div>
                 <ContentHeader title='Pagamentos'small='Cadastro' />
                 <Content>
-                    Pagamentos
+                    <Tabs>
+                        <TabsHeader>
+
+                        </TabsHeader>
+                        
+                        <TabsContent>
+
+                        </TabsContent>
+                    </Tabs>
                 </Content>
             </div>
         )
