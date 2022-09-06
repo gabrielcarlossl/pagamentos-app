02f9a5f796579a371400f032c2727f2b4335c8c7
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-09-06 08:50:22 -0300

    adicionado o mapeamento das actions no reducer

--- frontend/src/common/tab/tabActions.js
+++ frontend/src/common/tab/tabActions.js
@@ -3,7 +3,7 @@
 // ação de selecionar a aba e ação de mostrar qual aba está visivel
 
 export function selectTab(tabId){
-    console.log(tabId)
+    
     // devido a função ser um action creator, ela cria a ação e retorna um objeto
     // com um tipo e um payload que será o id da tab selecionada 
     return{
--- frontend/src/common/tab/tabHeader.jsx
+++ frontend/src/common/tab/tabHeader.jsx
@@ -1,4 +1,9 @@
 import React, {Component} from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+
+import { selectTab } from "./tabActions";
+
 
 //Estrutura para renderizar uma unica aba
 class TabHeader extends Component {
@@ -6,7 +11,9 @@ class TabHeader extends Component {
         return(
                         //  
             <li>
-                <a href='javascript:;' data-toggle='tab' data-target={this.props.target}>
+                <a href='javascript:;' data-toggle='tab'
+                    onClick={()=> this.props.selectTab(this.props.target)} // mapear o evento no onclick, para chamar o this.props.selectTab, passando o target que será o conteudo
+                    data-target={this.props.target}>
                     <i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
                 </a>
             </li>
@@ -14,4 +21,13 @@ class TabHeader extends Component {
     }
 }
 
-export default TabHeader
+// o componente tabHeader precisa acesso ao estado então é preciso fazer
+// o metodo mapStateToProps
+const mapStateToProps = state =>({tab: state.tab})
+// para fazer o mapeamento dos actions creators e que seja disparados
+// e estajam disponiveis pela propriedade do componenete é preciso do mapDispatchToProps
+// o dispatch chama a função action creators, a resposta é a action e a action é passada ao reducer para que ele evolua o estado 
+// assim o componenete é renderizado novamente mostrando o novo estado da aplicação
+
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TabHeader)
