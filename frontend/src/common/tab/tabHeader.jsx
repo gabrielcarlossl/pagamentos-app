f1ad1c3aa4e60d8b9231f590c7d163a57beb70ee
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-09-06 08:55:41 -0300

    adicionado função que edita o estilo quando a aba está selecionada

--- frontend/src/common/tab/tabHeader.jsx
+++ frontend/src/common/tab/tabHeader.jsx
@@ -8,9 +8,12 @@ import { selectTab } from "./tabActions";
 //Estrutura para renderizar uma unica aba
 class TabHeader extends Component {
     render(){
+
+        // constante para saber se foi selecionado a tab, o tab é uma string la no estado do redux, que tem um selected que é string, se ele for igual ao target entaõ sera verdadeiro e assim altera a classe do li
+        const selected = this.props.tab.selected === this.props.target
         return(
-                        //  
-            <li>
+                        //  para refletior o visual quando a tab for selecionada será criado uma classe css
+            <li className={selected ? 'active' : ''}>
                 <a href='javascript:;' data-toggle='tab'
                     onClick={()=> this.props.selectTab(this.props.target)} // mapear o evento no onclick, para chamar o this.props.selectTab, passando o target que será o conteudo
                     data-target={this.props.target}>
