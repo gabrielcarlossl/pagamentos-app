d43b8b86ed61b78fed4abe49820f0335129486a9
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-09-08 10:48:14 -0300

    add billingCycle reducer

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from "../common/tab/tabReducer";
+import BillingCycleReducer from "../billingCycle/billingCycleReducer";
 
 // definir o rootReducers, ele será a combinação de todos os reducers da aplicação 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
-    tab: TabReducer // TabReducer agora é responsavel pela parte do estado dos components TAB
+    tab: TabReducer, // TabReducer agora é responsavel pela parte do estado dos components TAB
+    billingCycle: BillingCycleReducer
 })
 
 export default rootReducer
