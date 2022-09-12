5cb401b733dce293df0afc1bae91489ba80cffa0
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-09-12 14:21:36 -0300

    editado função de preencher formulario

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+import { toastr } from "react-redux-toastr";
+import {reset as resetForm} from 'redux-form'
+import { showTabs, selectTab } from "../common/tab/tabActions";
+
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
@@ -11,8 +15,25 @@ export function getList() {
 
 // fazer a chamada da api do backend fazendo o post dos valores o objeto do metodo create
 export function create(values){
-    axios.post(`${BASE_URL}/billingCycles`, values)
-    return {
-        type: 'TEMP'
+
+    return dispatch =>{
+
+        axios.post(`${BASE_URL}/billingCycles`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação Realizada com sucesso!')
+                dispatch([
+                    resetForm('billingCycleForm'),
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList', 'tabCreate')
+                ])
+            })
+            .catch(e=> {
+                e.response.data.errors.forEach(error => toastr.error('Erro', error) 
+                    
+                )
+            })
     }
+    
+    
 }
