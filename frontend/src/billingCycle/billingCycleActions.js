9df82694db934b99cef2603f712ed7037736c25f
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-09-13 15:08:58 -0300

    editado formulario, add botão, editar formulario

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,39 +1,45 @@
 import axios from "axios";
 import { toastr } from "react-redux-toastr";
-import {reset as resetForm} from 'redux-form'
+import { reset as resetForm, initialize } from "redux-form";
 import { showTabs, selectTab } from "../common/tab/tabActions";
 
-const BASE_URL = 'http://localhost:3003/api'
+const BASE_URL = "http://localhost:3003/api";
 
 export function getList() {
-    const request = axios.get(`${BASE_URL}/billingCycles`)
-    return {
-        type: 'BILLING_CYCLES_FETCHED',
-        payload: request
-    }
+  const request = axios.get(`${BASE_URL}/billingCycles`);
+  return {
+    type: "BILLING_CYCLES_FETCHED",
+    payload: request,
+  };
 }
 
 // fazer a chamada da api do backend fazendo o post dos valores o objeto do metodo create
-export function create(values){
-
-    return dispatch =>{
-
-        axios.post(`${BASE_URL}/billingCycles`, values)
-            .then(resp => {
-                toastr.success('Sucesso', 'Operação Realizada com sucesso!')
-                dispatch([
-                    resetForm('billingCycleForm'),
-                    getList(),
-                    selectTab('tabList'),
-                    showTabs('tabList', 'tabCreate')
-                ])
-            })
-            .catch(e=> {
-                e.response.data.errors.forEach(error => toastr.error('Erro', error) 
-                    
-                )
-            })
-    }
-    
-    
+export function create(values) {
+  return (dispatch) => {
+    axios
+      .post(`${BASE_URL}/billingCycles`, values)
+      .then((resp) => {
+        toastr.success("Sucesso", "Operação Realizada com sucesso!");
+        dispatch([
+          resetForm("billingCycleForm"),
+          getList(),
+          selectTab("tabList"),
+          showTabs("tabList", "tabCreate"),
+        ]);
+      })
+      .catch((e) => {
+        e.response.data.errors.forEach((error) => toastr.error("Erro", error));
+      });
+  };
 }
+
+// quando mostrar a tela de alterar ele vai selecionar a aba de alterar e vai inicializar o formulario, passando como paramentro o ciclo de pagamento
+
+export function showUpdate(billingCycle){
+    return [
+        showTabs('tabUpdate'),
+        selectTab('tabUpdate'),
+        initialize('billingCycleForm', billingCycle)
+    ]
+}
+
