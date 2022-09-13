4981fdb5ec548958c8d528b8d622d2d12743bbfb
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-09-13 13:53:21 -0300

    edição dos inputs do formulario

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,24 +1,25 @@
-import React, {Component} from "react";
-import {reduxForm, Field} from 'redux-form'
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import LabelAndInput from "../common/tab/form/labelAndInput";
 
 class BillingCycleForm extends Component {
-
-
-    render(){
-        const {handleSubmit} = this.props
-        return(
-            <form role='form' onSubmit={handleSubmit}>
-                <div className="box-body">
-                    <Field name="name" component='input'></Field>
-                    <Field name="month" component='input'></Field>
-                    <Field name="year" component='input'></Field>
-                </div>
-                <div className="box-footer">
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </div>
-            </form>
-        )
-    }
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form role="form" onSubmit={handleSubmit}>
+        <div className="box-body">
+          <Field name="name" component={LabelAndInput} label='Nome' cols='12 4' placeholder="Informe o nome"></Field>
+          <Field name="month" component={LabelAndInput} type='number' label='Mês' cols='12 4' placeholder="Informe o mês"></Field>
+          <Field name="year" component={LabelAndInput} type='number' label='Ano' cols='12 4' placeholder="Informe o ano"></Field>
+        </div>
+        <div className="box-footer">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
 
-export default reduxForm({form: 'billingCycleForm'})(BillingCycleForm)
+export default reduxForm({ form: "billingCycleForm" })(BillingCycleForm);
---
+++ frontend/src/common/tab/form/labelAndInput.jsx
@@ -0,0 +1,18 @@
+import Grid from "../../layout/grid";
+
+import React from "react";
+
+export default props  => (
+  <Grid cols={props.cols}>
+    <div className="form-group">
+      <label htmlFor={props.name}>{props.label}</label>
+      <input
+        {...props.input}
+        className="form-control"
+        placeholder={props.placeholder}
+        readOnly={props.readOnly}
+        type={props.type}
+      />
+    </div>
+  </Grid>
+);
